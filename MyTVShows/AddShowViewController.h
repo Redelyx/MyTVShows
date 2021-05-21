2a50d160b9179984568fc6d0c65407502b9e1ac8
Redelyx <alice.lv@live.it>
2021-05-21 22:22:42 +0200

    some auto layout

---
+++ MyTVShows/AddShowViewController.h
@@ -0,0 +1,17 @@
+//
+//  AddShowViewController.h
+//  MyTVShows
+//
+//  Created by Alice on 21/05/2021.
+//  Copyright © 2021 Alice. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface AddShowViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ MyTVShows/AddShowViewController.m
@@ -0,0 +1,33 @@
+//
+//  AddShowViewController.m
+//  MyTVShows
+//
+//  Created by Alice on 21/05/2021.
+//  Copyright © 2021 Alice. All rights reserved.
+//
+
+#import "AddShowViewController.h"
+
+@interface AddShowViewController ()
+
+
+@end
+
+@implementation AddShowViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
